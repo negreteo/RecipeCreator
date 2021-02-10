58e1d2bd14b3cf357eeb8a7f5a165c4b7a1abb54
Oscar Negrete <oscar.negrete09@gmail.com>
2021-02-10 17:18:00 -0500

    Updated recipes feature

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 # Recipe Creator
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.5.
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Roast Brussels Sprouts Kimchi',
+      'This is a recipe for roasted brussels sprouts',
+      'https://www.seriouseats.com/images/2015/10/20151019-brussels-sprouts-kimchi-roast-recipe-8-kenji-1500x1125.jpg'
+    ),
+    new Recipe(
+      'Roast Brussels Sprouts Kimchi',
+      'This is a recipe for roasted brussels sprouts',
+      'https://www.seriouseats.com/images/2015/10/20151019-brussels-sprouts-kimchi-roast-recipe-8-kenji-1500x1125.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
