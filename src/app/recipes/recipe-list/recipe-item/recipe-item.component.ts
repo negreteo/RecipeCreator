61dde0484404102a4608ed695e28865cefa8f684
Oscar Negrete <oscar.negrete09@gmail.com>
2021-02-14 17:14:37 -0500

    Passing recipe data from outside component using Input

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+   selector: 'app-recipe-item',
+   templateUrl: './recipe-item.component.html',
+   styleUrls: ['./recipe-item.component.scss'],
 })
 export class RecipeItemComponent implements OnInit {
+   // Gets or recieves data from outside component
+   @Input() recipe: Recipe;
 
-  constructor() { }
+   constructor() {}
 
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {}
 }
